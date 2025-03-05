537130b0501143dbfeb6c72da003426d3d2b04e9
Pintu <pintu@Mac.lan>
2025-03-05 05:55:39 +0530

    create sanity and add blogpost schema

---
+++ schemaTypes/blogPost.ts
@@ -0,0 +1,34 @@
+export default {
+    name: 'blogPost',
+    title: 'Blog Post',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'text',
+      },
+      {
+        name: 'author',
+        title: 'Author',
+        type: 'string',
+      },
+      {
+        name: 'date',
+        title: 'Date',
+        type: 'datetime',
+      },
+      {
+        name: 'mainImage',
+        title: 'Main Image',
+        type: 'image',
+        options: { hotspot: true },
+      },
+    ],
+  };
+  
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,5 +2,6 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import blogPost from './blogPost'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, author, category, blockContent,blogPost]
